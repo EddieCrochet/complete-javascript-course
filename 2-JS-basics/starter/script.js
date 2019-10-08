f5e0cb79bd2b244b3c42e1fb8b05206b7621515d
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-10-08 00:49:22 -0500

    data mutation and type coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,52 @@
-console.log('hello world!!!!!');
+/************
+ variable and data types
+ */
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
+var johnMark = "John and Mark";
+
+/*
+this wont work
+var if = 23;
+*/
+
+/*************
+ variable mutation and type coercion
+ */
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName +' '+ age);
+
+//apparently this method is cleaner code
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old '+
+    job + '. Is he married? ' + isMarried);
+
+//variable mutation
+age = 'twenty eight';
+
+alert(firstName + ' is a ' + age + ' year old '+
+    job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
