7e46bb709e69269ef6d28d42ffa82bd0243b2542
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-10-15 14:27:59 -0500

    OOPS!!! Forgot to return the percentage times the bill in my calculator function

--- 2-JS-basics/starter/CodingChallenge3.js
+++ 2-JS-basics/starter/CodingChallenge3.js
@@ -1,13 +1,17 @@
-var tip;
 var tipCalc = function(bill) {
+    var percentage;
     if (bill < 50) {
-        tip = .2;
+        percentage = .2;
     } else if (bill > 200) {
-        tip = .1;
+        percentage = .1;
     } else {
-        tip = .15;
+        percentage = .15;
     }
+    return percentage * bill;
 }
 
 var tipTotals = [tipCalc(124), tipCalc(48), tipCalc(268)];
 var billTotals = [tipTotals[0] + 124, tipTotals[1] + 48, tipTotals[2] + 268];
+
+console.log(tipTotals);
+console.log(billTotals);
