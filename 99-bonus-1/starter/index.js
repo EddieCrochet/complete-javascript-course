34698094ae652d6b8237c6f3eb2af40125237f32
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-10-24 19:39:37 -0500

    created a server and a listener or said server

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -4,4 +4,10 @@ const http = require('http');
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+    console.log('Someone did access the server.');
+})
+
+server.listen(1337, '127.0.0.1', () =>{
+    console.log('Listening for requests now!');
+});
