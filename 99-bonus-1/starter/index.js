2e4843a6eb6c70ed11b08884488b2ebbebbee8e7
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-10-28 17:26:53 -0500

    basics of sending a request and getting a response from a server with node

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -4,9 +4,12 @@ const http = require('http');
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
+
+//callback function gets called everytime someone accesses the server
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server.');
-})
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.end('This is the response!');
+});
 
 server.listen(1337, '127.0.0.1', () =>{
     console.log('Listening for requests now!');
