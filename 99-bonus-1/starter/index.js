d2fa6f6f900d329f09ca8a019cdfe9f10754d335
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-10-28 18:20:40 -0500

    adding if statements for different request and response possibilities.

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
@@ -7,8 +8,22 @@ const laptopData = JSON.parse(json);
 
 //callback function gets called everytime someone accesses the server
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end('This is the response!');
+
+    const pathName = url.parse(req.url, true).pathname
+    const query = url.parse(req.url, true).query;
+
+    console.log(url.parse(req.url, true));
+
+    if (pathName === '/products' || pathName === '/'){
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('This is the PRODUCTS page!');
+    } else if (pathName === '/laptop') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('This is the LAPTOP page!');
+    } else {
+        res.writeHead(404, { 'content-Type': 'text/html'});
+        res.end('URL was not found on the server!');
+    }
 });
 
 server.listen(1337, '127.0.0.1', () =>{
