eef9b328ba0716a4a2d206911d215824c7ae853c
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-11-10 12:53:38 -0600

    objects and properties

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -339,4 +339,28 @@ if (height =='23'){
     //////////////
     //Objects and properties
 
-    
+    //object literal
+    var john = {
+        firstName: 'John',
+        lastName: 'Smith',
+        birthYear: 1990,
+        family: ['Jane', 'Mark', 'Bob', 'Emily'],
+        job: 'teacher',
+        isMarried: false
+    };
+    console.log(john.firstName);
+    console.log(john['lastName']);
+    var x = 'birthYear';
+    console.log(john[x]);
+
+    //mutating data once again
+    john.job = 'designer';
+    john['isMarried'] = true;
+    console.log(john);
+
+    //new object syntax
+    var jane = new Object();
+    jane.name = 'Jane';
+    jane.birthYear = 1969;
+    jane['lastName'] = 'Smith';
+    console.log(jane);
