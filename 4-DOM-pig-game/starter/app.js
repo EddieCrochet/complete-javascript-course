d7323a431ecf06f43b1e9e66171f1db4b56ba05b
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-11-24 13:27:09 -0600

    updating scores and changing the active player

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,12 +9,6 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
-
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
@@ -35,6 +29,12 @@ btn();
 document.querySelector('.btn-roll').addEventListener('click', btn);
 */
 
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
 document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
@@ -52,5 +52,25 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. update the round score IF the rolled # was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 
 });
