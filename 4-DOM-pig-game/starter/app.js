149adcc3698fa21de47f505502df6cdd2b2b2b85
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-11-23 19:37:43 -0600

    got a random number from the dice and displayed the result and made all original values equal to zero.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,21 +9,48 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
+//var x = document.querySelector('#score-0').textContent;
 //dont set because its just to read the value and store later
 
-console.log(x);
+//console.log(x);
+
+/*
+function btn() {
+
+}
+btn();
+
+//don't pass in parenthesis for funtion because we are not calling it
+//we wanty the event listener to call it
+//this is a callback function
+document.querySelector('.btn-roll').addEventListener('click', btn);
+*/
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. need a random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update the round score IF the rolled # was NOT a 1
+
+});
