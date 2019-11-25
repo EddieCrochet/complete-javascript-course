13d3ac152945a8791bb125054c5bdf7657e76ee0
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-11-25 10:47:17 -0600

    created a js file to play with - made a rule that the player loses ALLpoints when rolling two 6 back to back

--- 4-DOM-pig-game/starter/challenges.js
+++ 4-DOM-pig-game/starter/challenges.js
@@ -1,35 +1,21 @@
 /*
-GAME RULES:
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
+1. A player looses his ENTIRE score when he rolls two 6 in a row. 
+After that, it's the next player's turn. (Hint: Always save the previous 
+    dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score,
+ so that they can change the predefined score of 100. (Hint: you can 
+    read that value with the .value property in JavaScript. This is a 
+    good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The 
+player looses his current score when one of them is a 1. (Hint: you will 
+    need CSS to position the second dice, so take a look at the CSS code 
+    for the first one.)
 */
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-//var x = document.querySelector('#score-0').textContent;
-//dont set because its just to read the value and store later
-
-//console.log(x);
-
-/*
-function btn() {
-
-}
-btn();
-
-//don't pass in parenthesis for funtion because we are not calling it
-//we wanty the event listener to call it
-//this is a callback function
-document.querySelector('.btn-roll').addEventListener('click', btn);
-*/
-
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, previousRoll;
 
 init();
 
@@ -37,20 +23,31 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
         //1. need a random number
         var dice = Math.floor(Math.random() * 6) + 1;
+        console.log(dice);
+        console.log(previousRoll);
 
         //2. display result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
-        //3. update the round score IF the rolled # was NOT a 1
-        if (dice !== 1) {
-            //Add score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        } else {
-            //Next player
+        if (dice === 6 && previousRoll === 6) {
+            //correct score of active playerS
+            scores[activePlayer] = 0;
+            // Update UI
+            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
             nextPlayer();
+        } else {
+            //3. update the round score IF the rolled # was NOT a 1
+            if (dice !== 1) {
+                previousRoll = dice;
+                //Add score
+                roundScore += dice;
+                document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            } else {
+                //Next player
+                nextPlayer();
+            }
         }
     }
 });
@@ -79,6 +76,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    previousRoll = 0;
     roundScore = 0;
 
     document.getElementById('current-0').textContent = '0';
@@ -116,4 +114,3 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 }
-
