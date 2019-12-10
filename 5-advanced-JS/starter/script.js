8713b524fb80baab7f2c41ed259968a2ca6998c0
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-12-10 14:02:09 -0600

    adding functions to prototype properties

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -14,4 +14,23 @@ var Person = function(name, yearOfBirth, job) {
     this.job = job;
 }
 
-var john = new Person('John', 1990, 'teacher')
+//this way you dan't have to load every method
+//into the constructor and into every objects
+//yet every created object has access to the prototype methods
+Person.prototype.calculateAge = function() {
+    console.log(2019 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
