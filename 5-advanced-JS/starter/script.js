72e50583d5848299dcc9fe7df7f8d3d07d22fb06
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-12-18 17:28:12 -0600

    primitives vs objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -61,4 +61,37 @@ var jane = Object.create(personProto,
 
     //Primitives vs Objects
 
-    
+    //primitives
+    var a =23;
+    var b = a;
+    a = 46;
+    console.log(a, b);
+
+    //objects
+    var obj1 = {
+        name: 'John',
+        age: 26
+    };
+    var obj2 = obj1;
+    obj1.age = 30;
+    //both log 30 because obj2 is not a new object
+    //it is just a new reference
+    console.log(obj1.age);
+    console.log(obj2.age);
+
+    //functions
+    var age = 27;
+    var obj = {
+        name: 'Jonas',
+        city: 'Lisbon'
+    };
+
+    function change(a, b) {
+        a = 30;
+        b.city = 'San Francisco';
+    }
+
+    change(age, obj);
+
+    console.log(age);
+    console.log(obj.city);
