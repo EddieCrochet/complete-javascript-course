c111b5093f0a0589f82669a5d1576b199212cd06
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-01-08 15:07:01 -0600

    Finished up with closure theorizing

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -211,6 +211,50 @@ var jane = Object.create(personProto,
     }
 
     var retirementUS = retirement(66);
+    var retirementGermany = retirement(65);
+    var retirementIceland = retirement(67);
+
+    retirementGermany(1990);
+    retirementIceland(1990);
     retirementUS(1990);
 
-    retirement(66)(1990);
+    //retirement(66)(1990);
+
+    /*
+ function interviewQuestion(job) {
+        if ( job === 'designer') {
+            return function(name) {
+                console.log(name + 
+                    ', can you please explain what UX design is?');
+            }
+        } else if (job === 'teacher') {
+            return function(name) {
+                console.log(name + 
+                    'what subject do you teach?');
+            }
+        } else {
+            return function(name) {
+                console.log('Hello ' + name +
+                ' what do you do?');
+            }
+        }
+    }
+    */
+
+//rewrite the above, but using closures
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + 
+                ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log(name + 
+                ', what subject do you teach?');
+        } else {
+            console.log('Hello ' + name +
+                ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('Mark');
