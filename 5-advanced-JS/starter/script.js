04ed8b1968a30567fa4a42eaeebc6db046d46222
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-01-08 18:31:28 -0600

    Bind, call, and apply

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -242,6 +242,7 @@ var jane = Object.create(personProto,
     */
 
 //rewrite the above, but using closures
+/*
 function interviewQuestion(job) {
     return function(name) {
         if (job === 'designer') {
@@ -258,3 +259,22 @@ function interviewQuestion(job) {
 }
 
 interviewQuestion('teacher')('Mark');
+*/
+
+///////////////
+//Bind, call, and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style ==='friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay);
+        }
+    }
+}
+
+john.presentation('friendly', 'morning');
