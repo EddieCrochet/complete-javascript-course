c4be4ba65a3f51ba76bd6c53dcc1cb4883eb409a
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-01-16 17:08:41 -0600

    bind, call, and apply methods

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -105,9 +105,9 @@ var jane = Object.create(personProto,
     var years = [1990, 1965, 1937, 2005, 1998];
 
     function arrayCalc(arr, fn) {
-        var arrRe = [];
+        var arrRes = [];
         for(var i = 0; i < arr.length; i++) {
-            arr.push(fn(arr[i]));
+            arrRes.push(fn(arr[i]));
         }
         return arrRes;
     }
@@ -273,9 +273,14 @@ var john = {
     job: 'teacher',
     presentation: function(style, timeOfDay) {
         if (style === 'formal') {
-            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+            console.log('Good ' + timeOfDay 
+            + ', Ladies and gentlemen! I\'m ' + this.name 
+            + ', I\'m a ' + this.job + ' and I\'m ' + this.age 
+            + ' years old.');
         } else if (style ==='friendly') {
-            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay);
+            console.log('Hey! What\'s up? I\'m ' + this.name 
+            + ', I\'m a ' + this.job + ' and I\'m ' + this.age 
+            + ' years old. Have a nice ' + timeOfDay);
         }
     }
 };
@@ -296,3 +301,35 @@ john.presentation.call(emily, 'friendly', 'afternoon');
 var johnFriendly = john.presentation.bind(john, 'friendly');
 
 johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+
+emilyFormal('afternoon');
+
+
+
+//redo some old code for real world example
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+    function arrayCalc(arr, fn) {
+        var arrRes = [];
+        for(var i = 0; i < arr.length; i++) {
+            arrRes.push(fn(arr[i]));
+        }
+        return arrRes;
+    }
+
+    function calculateAge(el) {
+        return 2019 - el;
+    }
+
+    function isLegal(limit, el) {
+        return el >= limit;
+    }
+
+    var ages = arrayCalc(years, calculateAge);
+    var fullJapan = arrayCalc(ages, isLegal.bind(this, 20));
+    console.log(ages);
+    console.log(fullJapan);
