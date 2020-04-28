71e1197bccc8fc5de0b604004161159be885034c
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-04-28 13:23:02 -0500

    Simply playing with modules and theorizing how they will work together in my application

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -6,7 +6,29 @@ var budgetController = (function() {
     }
     return {
         publicTest: function(b) {
-            console.log(add(b));
+            //console.log(add(b));
+            //now that we are not accessing directly from the console
+            //we need to return the add function, instead of console.logging it
+            return add(b);
         }
     }
 })();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+//budgetController.publicTest()
+//dont do this cause we would have to change name between modules
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+            //this way is only way we can have access to z variable from the 
+            //outside once the original function returns
+        }
+    }
+
+})(budgetController, UIController);
