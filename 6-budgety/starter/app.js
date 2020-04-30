bfab5e396eeb953a4915f6331741c73e52aa3de7
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-04-30 17:58:06 -0500

    Adding the new input from data list to the UI- for some reason it is saying my variable is not an element

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -74,7 +74,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
@@ -85,6 +87,36 @@ var UIController = (function() {
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value
             };
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            // 1. create html string with placeholder txt
+
+            //check out what is passed to the html within the %% parameters
+            //this is to MARK our data being passed
+            if (type === 'inc') {
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix">' + 
+                '<div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn">' + 
+                '<i class="ion-ios-close-outline"></i></button> </div> </div></div>'
+            } else if (type === 'exp') {
+                element = DOMStrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix">' +
+                    '<div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete">' +
+                    '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div></div>'
+            }
+            
+            // 2. replace placeholder txt with some actual data
+
+            //searches for a string and replaces that string with data that we put into the method
+            //this actually passes the data into the markings
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // 3. insert the html into the dom
+            document.querySelector(element).insertAdjacentElement('beforeend', newHtml);
         },
 
         getDOMStrings: function() {
@@ -122,6 +154,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //3. add the new item to UI as well
+        UICtrl.addListItem(newItem, input.type);
 
         //4. calculate the budget
 
