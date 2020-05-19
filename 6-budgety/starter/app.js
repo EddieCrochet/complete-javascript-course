c06e0ef5c68c428ae1a92bda05c4750fbcd7b380
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-05-19 09:51:55 -0500

    added a function to display the month and year on page load

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -175,7 +175,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -316,6 +317,20 @@ var UIController = (function() {
 
         },
 
+        displayMonth: function() {
+            var now, year, month, months;
+            //need to use the 'new' keyword on object constructors
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 
+        'July', 'August', 'September', 'October', 'November', 'December'];
+
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMStrings: function() {
             return DOMStrings;
         }
@@ -426,6 +441,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth();
             //calling next function to reset everything to zero for page reload
             UICtrl.displayBudget({
                 budget: 0,
