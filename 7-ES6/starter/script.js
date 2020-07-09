b3dc1fd5a2b6bc49010c58414a45c820c10d6e46
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-07-09 16:45:25 -0500

    template literals in strings

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -21,6 +21,7 @@ console.log(name6);
 
 */
 
+/*
 //ES5
 function driversLicense5(passedTest) {
     if (passedTest) {
@@ -67,3 +68,53 @@ for (var i = 0; i < 5; i++) {
 //scope makes this i let be 23 but 5 if var
 //does not change the var outside the block
 console.log(i);
+
+*/
+
+///////////////////////
+//Blocks and IIFEs
+
+/*
+{
+    //this is a block
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+console.log(a + b);
+//error because the vars are not accessible outside the block
+//DATA PRIVACY INSTEAD OF AN IIFE
+console.log(c);
+//no error because var is always accessible - NO DATA PRIVACY
+
+*/
+
+
+/////////////////
+//Strings
+
+let firstName = 'john';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+function calcAge(year) {
+    return 2019 - year;
+}
+
+// ES6
+//no more plusses and quotes bla bla
+//back ticks
+
+console.log(`This is ${firstName} ${lastName}.
+Today he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('th'));
+console.log(n.includes('8'));
+console.log(`${firstName} `.repeat(5));
+
+
+
+
+
