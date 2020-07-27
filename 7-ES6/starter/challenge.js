e7b6f8ba640ba36a422f3fe039604736c4495b99
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-07-27 12:28:42 -0500

    spec 1 complete - tree density of all parks in town

--- 7-ES6/starter/challenge.js
+++ 7-ES6/starter/challenge.js
@@ -9,11 +9,10 @@ class Park {
         this.age = age;
     }
 
-    /*
     getTreeDensity() {
-        for (var cur)
+        var treeDensity = this.numberOfTrees/this.squareFeet;
+        return `${this.name} has a tree density of ${treeDensity} trees per square km.`;
     }
-    */
 }
 
 class Street {
@@ -26,9 +25,13 @@ class Street {
 
 const park1 = new Park('Greene Park', 467, 1023, 43);
 const park2 = new Park('Devlin Park', 23, 223, 13);
-const park3 = new Park('Kneaele Park', 744, 3438, 36);
+const park3 = new Park('Kneaele Park', 5437, 3438, 36);
 const park4 = new Park('Gourdge Park', 7, 10523, 437);
 
 var parksArr = [];
 parksArr.push(park1, park2, park3, park4);
-console.log(parksArr);
+//console.warn(parksArr);
+
+parksArr.forEach(function(cur) {
+    console.log(cur.getTreeDensity());
+});
