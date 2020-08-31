e64eaa8403cf90c99585c5173369fa80ad05c3fc
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-08-31 18:22:50 -0500

    clearly working with modules

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,5 @@
-// Global app controller
-import x from './test.js';
-const num = 23;
+import string from './models/Search';
+//import {add, multiply, ID} from './views/searchView';
+import * as searchView from './views/searchView';
 
-console.log(`I imported ${x} from another module called test.js! Variable num is ${num}`);
+console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and  ${searchView.multiply(searchView.ID, 5)}.... ${string}`)
---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,1 @@
+export default 'I aman exported string';
