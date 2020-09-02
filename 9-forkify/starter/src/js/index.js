8ea3f60cb074c8168f3da62c08dd3785301debe1
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-09-01 21:55:26 -0500

    api calling

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,20 @@
+import axios from 'axios';
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`)
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults("pasta");
+
+/*
 import string from './models/Search';
 //import {add, multiply, ID} from './views/searchView';
 import * as searchView from './views/searchView';
 
-console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and  ${searchView.multiply(searchView.ID, 5)}.... ${string}`)
+console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and  ${searchView.multiply(searchView.ID, 5)}.... ${string}`);
+*/
