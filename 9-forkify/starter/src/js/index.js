1b5f8144990e4c1443754f4d5580c6e7f81564ed
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-09-02 18:31:35 -0500

    built the search controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,8 +1,37 @@
+//GLOBAL CONTROLLER
+
 import Search from'./models/Search';
 
-const search = new Search("pizza");
-console.log(search);
-search.getResults();
+//global state of the app
+// - search object
+// - current recipe object
+// - shopping list object
+// - liked recipes
+// all above data will be stored in one central variabloe that we can access thru our controller
+const state = {};
+
+const controlSearch = async () => {
+    // 1) get query from the view
+    const query = 'pizza' //TODO
+
+    if (query) {
+        // 2) new search object and add it to state
+        state.search = new Search(query);
+
+        // 3) prepare UI for results
+
+        // 4) search for recipes
+        await state.search.getResults();
+        
+        // 5) render results on UI
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
 
 /*
 import string from './models/Search';
