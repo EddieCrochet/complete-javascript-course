63b18a5fea3342fc2d71fe569749f95d74091dac
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-09-04 12:52:24 -0500

    Finished building the Search View.

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -14,6 +14,25 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+//17 is the sweet spot for our pages layot for titles
+// 'pasta with tomato and spinach"
+const limitRecipeTitle = (title, limit = 17) => {
+    //variable to store the array of words for the new title
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        //return the final result
+        return `${newTitle.join(' ')} ...`;
+    } 
+    return title;
+};
+
 const renderRecipe = recipe => {
     //make a variable containing the markup we want to append
     //send the html you want to add to a template string in a variable
@@ -24,7 +43,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="Test">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
